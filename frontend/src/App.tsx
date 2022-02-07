7861eb467064e1f2f5597004147a46d35c9522b0
Amber Holland <68969731+amshholland@users.noreply.github.com>
2022-02-07 17:50:12 -0500

    Removed unused component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import { BudgetTable } from './components/BudgetTable';
-import { Customize } from './components/Customize';
 import { EditBudget } from './components/EditBudget';
 import { ExistingBudgetData } from './components/ExistingBudgetData';
 import { Goals } from './components/Goals';
